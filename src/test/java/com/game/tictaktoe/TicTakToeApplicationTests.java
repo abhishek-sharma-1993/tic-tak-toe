944a10ce477b3e282534c63f4109ea82873d6ff9
abhishek-sharma-1993 <abhishek.hcst15@gmail.com>
2024-10-13 22:34:46 +0530

    Configured maven project

---
+++ src/test/java/com/game/tictaktoe/TicTakToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.tictaktoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTakToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
